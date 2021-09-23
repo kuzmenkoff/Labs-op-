a69f35307d2896e7c2e79de75f32703c2f44a096
kuzmenkoff <47691438+kuzmenkoff@users.noreply.github.com>
2021-09-23 11:48:58 +0300

    Add files via upload

---
+++ Lab_1(c++)/LAb/LAb.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Ведите три числа, из которых хотите найти среднее геометрическое." << endl;
+	long double a, b, c, d, x, y;
+	std::cin >> a; 
+	std::cin >> b; 
+	std::cin >> c; 
+	d = pow(a * b * c, 1.0 / 3.); 
+	x = modf(d, &y);
+	std::cout << "Целая и дробная части числа " << d << " равны " << y << " и " << x;
+
+	return 0;
+}
