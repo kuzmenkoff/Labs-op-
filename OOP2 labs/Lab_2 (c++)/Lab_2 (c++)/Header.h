6ddfd7ae10d8b3bb07c0717b9ac6602b6fc38d6d
kuzmenkoff <47691438+kuzmenkoff@users.noreply.github.com>
2022-03-11 22:16:49 +0200

    Add files via upload

---
+++ OOP2 labs/Lab_2 (c++)/Lab_2 (c++)/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void CreateFile(int n);
+bool CheckAge(string date);
+void FileOutput1();
+void Sort();
+bool CheckAge2(string date);
+void FileOutput2();
---
+++ OOP2 labs/Lab_2 (c++)/Lab_2 (c++)/Lab_2 (c++).cpp
@@ -0,0 +1,16 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Select mode:\n1) Create new file\n2) Change old file\n3) Just use last one\n";
+    cin >> n;
+    CreateFile(n);
+    FileOutput1();
+    Sort();
+    FileOutput2();
+}
